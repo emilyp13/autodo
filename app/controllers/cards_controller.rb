268bacf5ddd6892527659b35d898789efa3cb40f
emilyp13 <elpair@gmail.com>
2016-07-18 22:52:33 -0400

    added editing and deleting to card model

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -16,6 +16,28 @@ class CardsController < ApplicationController
     end
   end
 
+  def edit
+    @card = Card.find(params[:id])
+    @list = @card.list
+  end
+
+  def update
+    @card = Card.find(params[:id])
+    @list = List.find(params[:list_id])
+    if @card.update(card_params)
+      redirect_to lists_path
+    else
+      flash[:errors] = @card.errors.full_messages.join(', ')
+      render :edit
+    end
+  end
+
+  def destroy
+    @card = Card.find(params[:id])
+    @card.destroy
+    redirect_to lists_path
+  end
+
   private
   def card_params
     params.require(:card).permit(:text, :completed)
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   resources :lists do
-    resources :cards
+    resources :cards, only: [:new, :index, :create, :show, :update]
   end
 
-  resources :cards, only: [:index, :create]
+  resources :cards, only: [:index, :create, :edit, :show, :destroy]
   root to: 'lists#index'
 end
